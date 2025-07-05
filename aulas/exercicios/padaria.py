90f38552e87b6563d2c13252735153d64bda4e92
Edenilson <freitaseddie126@gmail.com>
2025-07-05 12:52:16 -0300

    Usei array para fazer essa tarefa

--- aulas/exercicios/padaria.py
+++ aulas/exercicios/padaria.py
@@ -6,16 +6,36 @@ Você foi a uma padaria e comprou alguns itens:
 ● Café: R$2.80
 Você pagou com uma nota de R$20. Calcule quanto de troco você deve receber.
 """
+"""
 pao = 3.50
 leite = 4.20
 cafe = 2.80
 
-compra_total = pao + leite + cafe
+#compra_total = pao + leite + cafe
+compra_total = ["pao","leite","cafe"] # usando array para somar
 
 valor_pago = 20
 
 troco = valor_pago - compra_total
 
 print("Valor da compra :", compra_total)
+print("Valor a receber :", troco)
+"""
+# Usando array para somar essa lista
+pao = 3.50
+leite = 4.20
+cafe = 2.80
+
+# Lista com os valores dos produtos
+compra_total = [pao, leite, cafe]  
+
+# Somando os valores da lista com a função sum()
+soma_total = sum(compra_total)
+
+valor_pago = 20
+
+troco = valor_pago - soma_total
+
+print("Valor da compra :", soma_total)
 print("Valor a receber :", troco)
 
