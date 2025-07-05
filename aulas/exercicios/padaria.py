0143efbab7967519e2c7b36d8f3a422e7d70ad0c
Edenilson <freitaseddie126@gmail.com>
2025-07-05 12:39:58 -0300

    desafio padaria

---
+++ aulas/exercicios/padaria.py
@@ -0,0 +1,21 @@
+"""
+1. Calculando o Troco
+Você foi a uma padaria e comprou alguns itens:
+● Pão: R$3.50
+● Leite: R$4.20
+● Café: R$2.80
+Você pagou com uma nota de R$20. Calcule quanto de troco você deve receber.
+"""
+pao = 3.50
+leite = 4.20
+cafe = 2.80
+
+compra_total = pao + leite + cafe
+
+valor_pago = 20
+
+troco = valor_pago - compra_total
+
+print("Valor da compra :", compra_total)
+print("Valor a receber :", troco)
+
